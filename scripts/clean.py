b8d5a272a5ecdb619b9f2e776605ae898626a7d9
Maitysantanu <143122493+Maitysantanu@users.noreply.github.com>
2026-04-21 12:18:37 +0530

    Refactor clean.py for improved structure
    Reorganize code for better readability and maintainability.

--- scripts/clean.py
+++ scripts/clean.py
@@ -4,13 +4,10 @@ from modules.skill_extractor import extract_skills
 
 df = pd.read_csv("data/raw/resumes.csv")
 
-# Clean missing
 df = clean_missing(df)
 
-# Extract skills automatically
 df['skills'] = df['resume_text'].apply(lambda x: ", ".join(extract_skills(x)))
 
-# Save processed file
 df.to_csv("data/processed/cleaned_resumes.csv", index=False)
 
 print("Data cleaned + skills extracted")
